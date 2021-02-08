54bb6a0be8d93ac5cdd2a2e8d329a49929c05a12
JiaYongg <s10202579@connect.np.edu.sg>
2021-02-09 00:30:04 +0800

    Added hover function to "cart" icon

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-fetch('https://fakestoreapi.com/products?limit=5')
-    .then(res=>res.json())
-    .then(json=>console.log(json))
-
+/* PreLoader */
 
 function myFunction(){
     setTimeout(showPage, 3000)
@@ -11,3 +8,18 @@ function showPage(){
     document.getElementById("preloader").style.display = "none";
     document.getElementById("overall_content").style.display = "block";
 }
+
+/* Cart Hover */
+(function(){
+$("#cart").hover(function() {
+      $(".cart-items").fadeToggle("fast");
+    });
+})();
+
+
+
+/* API */
+/* YET TO IMPLEMENT */
+fetch('https://fakestoreapi.com/products?limit=5')
+    .then(res=>res.json())
+    .then(json=>console.log(json))
