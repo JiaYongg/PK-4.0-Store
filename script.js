2ad096b4844a6e2960ee3ae78c5d89912b4743f7
JordanChoi <s10208161@connect.np.edu.sg>
2021-02-17 00:29:21 +0800

    Added tooltip for featured products for UX

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/* Tooltip Enable */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 /* Global var */
 var currencyRate;
 
